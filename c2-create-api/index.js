6cd2dcbce3298401921028afb757636f32a08bc6
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-11-30 04:46:38 +0500

    Keep learning on routing of backend on Nov 30, 24

--- c2-create-api/index.js
+++ c2-create-api/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import {comments} from './comments.js';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello Usama!</h1>${data}`);
+})
 
+app.get('/posts', (req, res) => {
+    res.status(200).send({comments})
+})
 
-app.listeners(8000, () => {
+app.listen(8000, () => {
     console.log('Server is running!');
     
 })
