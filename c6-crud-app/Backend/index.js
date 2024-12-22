3864e5eae39f0a50ac16b5b1f5214f831ca754f9
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-12-22 06:08:31 +0500

    Topic is frontend is integrated with backend on process on dec 22, 24

--- c6-crud-app/Backend/index.js
+++ c6-crud-app/Backend/index.js
@@ -1,22 +1,129 @@
-// Extracted code
+import express from 'express';
+import dotenv from 'dotenv';
+import { taskRoutes } from './routes/taskRoutes.js';
+import { enums } from './constant/enum.js';
+import mongoose from 'mongoose';
 
-import express from "express";
-import cors from "cors";
-import { taskRoutes } from "./routes/taskRoutes.js";
-import dotenv from "dotenv";
+/*
+
+mongodb
+username: usamaisrar1190
+password: hxwqKnbEW8n5M22x
+
+*/
+
 const app = express();
 dotenv.config();
-app.use(cors());
+
 app.use(express.json());
 
-// routes
-app.get("/", (req, res) => {
-  res.send("welcome to bc");
-});
-app.use("/tasks", taskRoutes);
+app.use('/tasks', taskRoutes)
 
-// server
-const PORT = process.env.PORT;
+app.get('/', (req, res) => {
+    try {
+        res.status(200).send({ status: 200, message: enums.SUCCESS })
+    }
+    catch(error) {
+        res.status(400).send({ status: 400, message: enums.ERROR_MSG })
+    }
+})
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+/*  
+
+mongoose.connect(MONGODB_URI)
+.then(() => {
+    console.log('db connected successfully!');
+    
+})
+.catch((error) => {
+    console.log('db not connected!', error);
+    
+})
+
+*/
+
+
+const dbConnection = async() => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log('db connected successfully!');
+        
+    } catch (error) {
+        console.log('db not connected!', error);
+        
+    }
+}
+
+dbConnection();
+
+
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
-  console.log("server started");
-});
+    console.log(`Server running on port ${PORT}`);
+})
+
+
+
+
+
+
+
+
+
+/*
+
+// Extracted code
+
+import express from 'express'
+import dotenv from 'dotenv';
+import taskRoutes from './routes/taskRoutes.js';
+import mongoose from 'mongoose';
+// 
+const app = express();
+dotenv.config();
+
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.get('/',(req,res)=>{
+    res.send("welcome to backend")
+})
+
+app.use('/tasks',taskRoutes);
+
+
+// mongoose.connect(MONGODB_URI)
+// .then(()=>{
+//     console.log("db connected successfully")
+// })
+// .catch((error)=>{
+//     console.log("db not connected" , error);
+// })
+
+const dbConnection = async()=>{
+    try{
+        await mongoose.connect(MONGODB_URI)
+        console.log("db connected succesfully")
+    }
+    catch(error){
+        console.log("db is not connected",error)
+    }
+}
+dbConnection();
+
+
+
+
+app.listen(PORT,()=>{
+    console.log('server started')
+})
+
+
+*/
